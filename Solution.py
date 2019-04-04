6699c50553aecf2fe34596ff05e4cdac982862c3
Shlomi <vgtvgy1@gmail.com>
2019-04-04 11:14:55 +0300

    Working monitor mode at windows

--- Solution.py
+++ Solution.py
@@ -2,24 +2,74 @@ import platform
 import os
 import sys
 import psutil
+import datetime
+import time
 
 
 
 
-print(list(psutil.win_service_iter()))
+'''
+Windows function
+Log_file = Write to this file
+Returns dictionary of Service name and Service status
+'''
+def Win_SampleToLog(log_file):
+    dict = {}
+    for it in psutil.win_service_iter():
+        date = datetime.datetime.now()
+        service_name = it.name()
+        service_status = it.status()
+        #service_description = it.description()
+        line_to_write = "{}: {} {}\n".format(date, service_name,service_status)
+        log_file.write(line_to_write)
+        dict[service_name] = service_status
+    log_file.close()
+    return dict
+
+
+'''
+Diffirentiate between 2 samples.
+log_file = Log file to append the changes in the services status
+sample1 = Old sample
+sample2 = New sample
+Return dict 
+'''
+def DiffSamples(log_file, sample1, sample2):
+	for key, value in sample1.items():
+		date = datetime.datetime.now()
+		if key not in sample2:
+			str = "Service {} is found at sample 1 but not sample 2. This service probably was uninstalled".format(key)
+			print(str)
+			log_file.write(str+"\n")
+			log_file.flush()
+		elif value != sample2[key]:
+			str = "{}: Service '{}' changed status from '{}' to '{}'".format(date, key, value, sample2[key])
+			print(str)
+			log_file.write(str+"\n")
+			log_file.flush()
+
+    #for key, value in sample2.items():
+        #date = datetime.datetime.now()
+        #if key not in sample1:
+        	#str = "Service {} is found at sample 2 but not sample 1. This service probably was uninstalled".format(key)
+            #print(str)
+            #log_file.write(str)
 
 # Choose mode: manual or monitor
 if(len(sys.argv) <= 1):
 	print("Choose mode: monitor or manual")
 	exit()
 
-# Get seconds
+# Monitor mode
 if("monitor" == sys.argv[1]):
 	print("> Monitor mode")
 	if(len(sys.argv) <= 2):
 		print("Enter how much seconds to refresh monitor")
 		exit()
+    # Get seconds
 	seconds = sys.argv[2]
+	str = "> Monitor mode: Refresh rate of {} seconds".format(seconds)
+	print(str)
 elif("manual" == sys.argv[1]):
 	print("> Manual mode")
 else:
@@ -30,6 +80,17 @@ platform = platform.system()
 ###################################### Windows Platform ######################################
 if(platform == "Windows"):
 	print("> Windows detected")
+	status_log = open("statusLog.log", "a")
+	log_file = open("serviceList.log", "w")
+	dict = Win_SampleToLog(log_file)
+	while True:
+		#print("> Sampleing (1) to serviceList.log ...")
+		my_dict = Win_SampleToLog(open("serviceList.log", "w"))
+		#print("> Sleeping ...")
+		time.sleep(10) # Sleep for 10 seconds
+		#print("> Sampleing (2) to serviceList.log ...")
+		my_dict2 = Win_SampleToLog(open("serviceList.log", "w"))
+		DiffSamples(status_log, my_dict, my_dict2)
 
 
 
--- test.py
+++
@@ -1,9 +0,0 @@
-import platform
-import os
-import sys
-import psutil
-
-print(list(psutil.win_service_iter()))
-for it in psutil.win_service_iter():
-    print(it.name(), it.status())
-    
